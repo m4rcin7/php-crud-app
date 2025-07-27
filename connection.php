a3bfbca4da6ac1a6ac7013c5dd94a6dd40a31941
Marcin Kopatys <mrku1337@gmail.com>
2025-07-27 14:53:23 +0200

    creating db and navbar

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+
+$username = 'root';
+$conn = new mysqli('localhost', $username, '', 'php-crud-app');
+$conn->set_charset('utf8mb4');
--- index.php
+++ index.php
@@ -1,11 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP-Crud-App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
 </head>
+
 <body>
-    
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item">
+                <img src="https://cdn.pixabay.com/photo/2016/08/22/05/10/ant-1611374_1280.png" alt="site-logo">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    Home
+                </a>
+
+                <a class="navbar-item">
+                    About Us
+                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        Tools
+                    </a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            #1
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            #2
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            #3
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            #4
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light">
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
 </body>
+
 </html>
